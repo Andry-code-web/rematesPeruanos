6fb5984fdb244311cad8f0b124477e1c4cbd3133
Andry-code-web <73274663@certus.edu.pe>
2024-11-22 11:37:07 -0500

    coneccion_database

--- src/router/cliente.js
+++ src/router/cliente.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database/db')
 require('dotenv').config();
 
 /* LOGIN */
@@ -13,26 +14,53 @@ router.get('/registrar', (res, req) => {
 })
 
 router.post('/registrar', (res, req) => {
-    
+
 })
 
 /* RUTA INICIO */
-router.get('/', (req, res) =>{
-    res.render("inicio");
+router.get('/', async (req, res) => {
+    try {
+        const [remates] = await pool.query(`
+        SELECT 
+          r.id AS id,
+          r.ubicacion,
+          r.precios,
+          r.fecha_activacion,
+          r.estado,
+          r.descripcion,
+          r.categoria
+        FROM remates r
+      `);
+        // Mostrar los datos obtenidos en consola
+        console.log("Datos de remates obtenidos:", remates);
+
+        // Verifica el tipo de datos y estructura
+        if (Array.isArray(remates)) {
+            console.log(`Se obtuvieron ${remates.length} remates`);
+        } else {
+            console.log("Los datos de remates no están en el formato esperado.");
+        }
+
+        res.render('inicio', { remates });
+    } catch (error) {
+        console.error('Error fetching remates:', error);
+        res.status(500).send('Error al cargar los datos');
+    }
 });
 
+
 /* RUTA REMATES */
-router.get('/remates', (req, res) =>{
+router.get('/remates', (req, res) => {
     res.render("remates");
 });
 
 /* RUTA SUBASTAS */
-router.get('/subasta', (req, res) =>{
+router.get('/subasta', (req, res) => {
     res.render("subasta");
 });
 
 /* RUTA CONTACTOS */
-router.get('/contacto', (req, res) =>{
+router.get('/contacto', (req, res) => {
     res.render("contactos");
 });
 module.exports = router;
