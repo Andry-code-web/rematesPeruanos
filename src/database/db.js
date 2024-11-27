bf091c05eda1b9a84be9e148d7cc6f6fb9639ef4
Andry-code-web <73274663@certus.edu.pe>
2024-11-27 10:21:21 -0500

    sin_variables_v3

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password:'1234',
   database: 'remajud',
