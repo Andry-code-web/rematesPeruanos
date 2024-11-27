89c0673883dcbf7e317cd5a83189ae546ca94c62
Andry-code-web <73274663@certus.edu.pe>
2024-11-27 11:33:14 -0500

    con_variables_v1

--- src/database/db.js
+++ src/database/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password:'1234',
-  database: 'remajud',
-  port:  3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 3306,
 });
 
 // Validación de conexión
